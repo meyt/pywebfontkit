462b3e304c262a98bfbc3b8147b7b1d2ea0b4702
meyt <pasd3000@gmail.com>
2017-04-27 00:57:48 +0430

    fix some issues and compatible with python2

---
+++ README.rst
@@ -0,0 +1,43 @@
+PyWebFontKit
+============
+
+A simple python based font bundle generator for web.
+collect multiple SVG files and convert to `woff`, `ttf`, `svg`, `otf`, `eof`, and create `CSS` with `HTML` map.
+ 
+ 
+Requirements
+------------
+
+- fontforge
+- python (obviously)
+
+
+Instructions
+------------
+
+- Download
+
+..  code-block:: bash
+
+		git clone https://github.com/meyt/pywebfontkit.git
+		cd ./pywebfontkit
+		sudo python ./setup.py install
+
+Or install from pypi
+
+..  code-block:: bash
+
+		sudo pip install pywebfontkit
+
+
+- Put SVG files to your bundle path like this ``./bundles/myfontname/svg``
+		
+- Build
+
+..  code-block:: bash
+
+		pywebfontkit -f myfontname -p mycssprefix
+		
+
+P.S: On some distribution like Ubuntu, doesn't work with python3,
+so install ``python-fontforge`` and run it under python2.
--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
+readme = open('README.rst').read()
 
 setup(name='pywebfontkit',
-      version='0.1.1',
+      version='0.2.0',
       description='a simple python based font bundle generator for web.',
       url='http://github.com/meyt/pywebfontkit',
+      long_description=readme,
       author='Mahdi Ghane.g',
       license='GPLv3',
       keywords='pywebfontkit svg woff ttf fontforge otf eot css html webfont webfontgenerator',
@@ -14,7 +16,9 @@ setup(name='pywebfontkit',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: POSIX :: Linux',
         'Operating System :: Unix',
-        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Build Tools'
       ],
       scripts=['pywebfontkit'],
