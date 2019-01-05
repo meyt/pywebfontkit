c35b11881a8506a6c59ce456a97c6fd20abb893b
meyt <pasd3000@gmail.com>
2019-01-05 23:59:48 +0330

    v0.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pywebfontkit',
-    version='0.3.0',
+    version='0.4.0',
     description='a simple python based font bundle generator for web.',
     url='http://github.com/meyt/pywebfontkit',
     long_description=open('README.rst').read(),
